478e18baecaf6da1620d6305075bc7f262307165
Tekaoh <45337851+Tekaoh@users.noreply.github.com>
2020-04-14 21:07:37 -0400

    version bump

--- swg-auth/swg-auth.php
+++ swg-auth/swg-auth.php
@@ -3,7 +3,7 @@
   * Plugin Name: SWG Auth
   * Plugin URI: https://tekaohswg.github.io/swg-auth-wordpress.html
   * Description: Star Wars Galaxies Authentication for Wordpress
-  * Version: 0.1
+  * Version: 0.2
   * Author: Tekaoh
   * Author URI: https://tekaohswg.github.io
 */
