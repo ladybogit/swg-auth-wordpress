55fecd27803cef124ae0d9360df0d85d4df63c8f
Tekaoh <45337851+Tekaoh@users.noreply.github.com>
2020-04-17 13:28:42 -0400

    Add a setting to require approval for game access
    Not yet implemented though... Just there for now.

--- swg-auth/admin/admin.php
+++ swg-auth/admin/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+// Add Menu Pages
 add_action('admin_menu', 'swg_auth_admin_menus');
 function swg_auth_admin_menus(){
   add_menu_page(
@@ -31,8 +32,53 @@ function swg_auth_admin_menus(){
   );
 }
 function swg_auth_settings_html(){
+  if(!current_user_can('manage_options')){
+    return;
+  }
   include(plugin_dir_path(__FILE__) . 'swg-auth-settings-html.php');
 }
 function swg_auth_server_config_html(){
+  if(!current_user_can('manage_options')){
+    return;
+  }
   include(plugin_dir_path(__FILE__) . 'swg-auth-server-config-html.php');
+}
+
+// Add settings
+add_action('admin_init', 'swg_auth_settings');
+function swg_auth_settings(){
+  add_settings_section(
+    'swg-auth-general-settings',
+    'General Settings',
+    'swg_auth_general_settings_html',
+    'swg-auth-settings'
+  );
+  register_setting(
+    'swg-auth-general-settings',
+    'swg-auth-approval-required',
+    array(
+      'type' => 'boolean',
+      'description' => 'Whether approval is required for game access.',
+      'sanitize_callback' => '', // TODO: A callback function that sanitizes the option's value.
+      'show_in_rest' => false,
+      'default' => false
+    )
+  );
+  add_settings_field(
+    'swg-auth-approval-required',
+    'Require approval before an account can have game access.',
+    'swg_auth_approval_required_html',
+    'swg-auth-settings',
+    'swg-auth-general-settings',
+    array(
+      //'label_for' => '', // When supplied, the setting title will be wrapped in a <label> element, its for attribute populated with this value.
+      //'class' => '', // CSS Class to be added to the <tr> element when the field is output.
+    )
+  );
+}
+function swg_auth_general_settings_html($args){
+  echo '';
+}
+function swg_auth_approval_required_html($args){
+  ?><input type="checkbox" name="swg-auth-approval-required" <?php echo (get_option('swg-auth-approval-required') == 'on') ? 'checked' : '' ?>><?php
 }
--- swg-auth/admin/swg-auth-settings-html.php
+++ swg-auth/admin/swg-auth-settings-html.php
@@ -1,4 +1,9 @@
+<?php settings_errors(); ?>
 <div class="wrap">
   <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
-  <p>Welcome to SWG Auth for Wordpress. Your plugin is functioning normally.</p>
+  <form action="options.php" method="post">
+    <?php settings_fields( 'swg-auth-general-settings' ); ?>
+    <?php do_settings_sections( 'swg-auth-settings' ); ?>
+    <?php submit_button( 'Save Settings' ); ?>
+  </form>
 </div>
