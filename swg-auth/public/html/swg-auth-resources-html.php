28776ca852f1991773f7663cd5364008755c2614
Tekaoh <45337851+Tekaoh@users.noreply.github.com>
2020-05-02 20:30:21 -0400

    Improving the single resource page

--- swg-auth/public/html/swg-auth-resources-html.php
+++ swg-auth/public/html/swg-auth-resources-html.php
@@ -1,6 +1,7 @@
 <?php
 
 $connection = swg_auth_oci_connect();
+
 $statement = oci_parse( $connection, "SELECT * FROM CLOCK" );
 $results = oci_execute( $statement );
 $clock = oci_fetch_array( $statement );
@@ -11,24 +12,63 @@ $format = get_option( 'date_format' );
 if ( isset( $_GET[ 'display' ] ) && $_GET [ 'display' ] === 'single' && isset( $_GET[ 'resource-name' ] ) ) :
   $statement = oci_parse( $connection, "SELECT * FROM RESOURCE_TYPES WHERE RESOURCE_NAME = '" . $_GET[ 'resource-name' ] . "'");
   $results = oci_execute( $statement );
-  $result = oci_fetch_array( $statement, OCI_ASSOC + OCI_RETURN_NULLS )
+  $result = oci_fetch_array( $statement, OCI_ASSOC + OCI_RETURN_NULLS );
 ?>
 
-<img src="<?php echo plugins_url(); ?>/swg-auth/public/img/resources/<?php echo $resources[ $result[ 'RESOURCE_CLASS' ] ][ 'image' ] ?>">
-<?php echo $result[ 'RESOURCE_NAME' ]; ?>
-<?php echo end( $resources[ $result[ 'RESOURCE_CLASS' ] ][ 'classes' ] ); ?>
-<?php
-  foreach ( swg_auth_parse_resource_attributes( $result[ 'ATTRIBUTES' ] ) as $attribute => $value ) {
-    echo $attribute . ': ' . $value . '<br />';
-  }
-?>
-<?php
-  foreach ( swg_auth_parse_fractal_seeds( $result[ 'FRACTAL_SEEDS' ] ) as $planet ) {
-    echo $planet . '<br />';
-  }
-?>
+<div class="swg-auth-single-resource-page">
+  <div class="swg-auth-resource-icon">
+    <img src="<?php echo plugins_url(); ?>/swg-auth/public/img/resources/<?php echo $resources[ $result[ 'RESOURCE_CLASS' ] ][ 'image' ] ?>">
+  </div>
+  <div class="swg-auth-resource-name">
+    <?php echo $result[ 'RESOURCE_NAME' ]; ?>
+  </div>
+  <div class="swg-auth-resource-class-breadcrumbs">
+    <?php
+      $last_class = end( $resources[ $result[ 'RESOURCE_CLASS' ] ][ 'classes' ] );
+      foreach ( $resources[ $result[ 'RESOURCE_CLASS' ] ][ 'classes' ] as $class ) {
+        if ( $class === $last_class ){
+          echo $class;
+        } else {
+          echo $class . ' > ';
+        }
+      }
+    ?>
+  </div>
+  <div class="swg-auth-resource-attributes">
+    Attributes:<br />
+    <?php
+      foreach ( swg_auth_parse_resource_attributes( $result[ 'ATTRIBUTES' ] ) as $attribute => $value ) {
+        echo $attribute . ': ' . $value . '<br />';
+      }
+    ?>
+  </div>
+  <div class="swg-auth-resource-planets">
+    Available On:<br />
+    <?php
+      foreach ( swg_auth_parse_fractal_seeds( $result[ 'FRACTAL_SEEDS' ] ) as $planet ) {
+        echo $planet . '<br />';
+      }
+    ?>
+  </div>
+  <div class="swg-auth-resource-deplete-time">
+    <?php
+      if ( $result[ 'DEPLETED_TIMESTAMP' ] > $clock[ 'LAST_SAVE_TIME' ] ) {
+        $seconds_left = $result[ 'DEPLETED_TIMESTAMP' ] - $clock[ 'LAST_SAVE_TIME' ];
+        $depletes_on = strtotime( $clock[ 'LAST_SAVE_TIMESTAMP' ] ) + $seconds_left;
+        echo 'Depletes On:<br />' . wp_date( $format, $depletes_on );
+      } elseif ( $result[ 'DEPLETED_TIMESTAMP' ] <= $clock[ 'LAST_SAVE_TIME' ] ) {
+        $seconds_ago = $clock[ 'LAST_SAVE_TIME' ] - $result[ 'DEPLETED_TIMESTAMP' ];
+        $depleted_on = strtotime( $clock[ 'LAST_SAVE_TIMESTAMP' ] ) - $seconds_ago;
+        echo 'Depleted On:<br />' . wp_date( $format, $depleted_on );
+      } else {
+        echo 'Oops. Something went wrong with the depletion calculation...';
+      }
+    ?>
+  </div>
+</div>
 
-<?php else :
+<?php
+  else :
   $statement = oci_parse( $connection, "SELECT * FROM RESOURCE_TYPES WHERE RESOURCE_NAME NOT LIKE '@%' AND RESOURCE_CLASS NOT LIKE 'space%'" );
   $results = oci_execute( $statement );
 ?>
